ec66d9b22e2e03f761e8fe20f1ee601679d40598
Baruh <isa_vita@abv.bg>
2014-06-29 22:00:35 +0300

    homework for telerik curse

---
+++ Homework/ToolsForDeveloping/ToolsForDeveloping/TextTemplateStudentClass.cs
@@ -0,0 +1,11 @@
+﻿
+public class Student
+{
+    public string FirstName { get; set; }
+    public string MiddleName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+    public string FacultyNumber { get; set; }
+    public string City { get; set; }
+}
+
